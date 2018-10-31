a44e48a47c68f58ff5edd85ed37237cf18d16b60
yosefsaputra <yosef.saputra@gmail.com>
2018-10-31 01:15:20 -0500

    To add Device and Profile classes

---
+++ app/src/main/java/com/example/yosef/shastore/model/components/Device.java
@@ -0,0 +1,8 @@
+package com.example.yosef.shastore.model.components;
+
+public class Device {
+    protected String name;
+    protected String friendlyName;
+    protected String UUID;
+    protected String key;
+}
---
+++ app/src/main/java/com/example/yosef/shastore/model/components/Profile.java
@@ -0,0 +1,6 @@
+package com.example.yosef.shastore.model.components;
+
+public class Profile {
+    protected String username;
+    protected String password; // TODO : make it secure
+}
