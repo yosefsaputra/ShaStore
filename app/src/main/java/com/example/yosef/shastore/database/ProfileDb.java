ae30b83308df0c7ec15788b201f18bb4b2edf32c
yosefsaputra <yosef.saputra@gmail.com>
2018-11-07 14:15:21 -0600

    To add getter and setter in ProfileDb. To add passwordSalt in ProfileDb. To define ProfileDb getProfileDb(String username) in ProfileDbDao

--- app/src/main/java/com/example/yosef/shastore/database/ProfileDb.java
+++ app/src/main/java/com/example/yosef/shastore/database/ProfileDb.java
@@ -8,4 +8,29 @@ public class ProfileDb {
     @PrimaryKey
     private String username;
     private String passwordHash;
+    private String passwordSalt;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
 }
--- app/src/main/java/com/example/yosef/shastore/database/ProfileDbDao.java
+++ app/src/main/java/com/example/yosef/shastore/database/ProfileDbDao.java
@@ -4,6 +4,7 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
 @Dao
@@ -16,4 +17,7 @@ public interface ProfileDbDao {
 
     @Delete
     void delete(ProfileDb profileDb);
+
+    @Query("SELECT * FROM profiledb WHERE username=:username")
+    ProfileDb getProfileDb(String username);
 }
