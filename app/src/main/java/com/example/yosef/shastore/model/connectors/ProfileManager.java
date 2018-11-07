95ecf4291393c2891a094c3b27bd5f8a5cfe261c
yosefsaputra <yosef.saputra@gmail.com>
2018-11-07 14:13:39 -0600

    To create a ProfileManager

---
+++ app/src/main/java/com/example/yosef/shastore/model/connectors/ProfileManager.java
@@ -0,0 +1,8 @@
+package com.example.yosef.shastore.model.connectors;
+
+public class ProfileManager {
+    public boolean authenticateProfile(String username, String passwordHash) {
+        // TODO: implement
+        return false;
+    }
+}
