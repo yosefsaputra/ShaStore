aa4333ae94e55dac05e3c982d1ae6ce8f72f7580
yosefsaputra <yosef.saputra@gmail.com>
2018-11-07 14:14:01 -0600

    To create a ProfileConverter

---
+++ app/src/main/java/com/example/yosef/shastore/database/converters/ProfileConverter.java
@@ -0,0 +1,16 @@
+package com.example.yosef.shastore.database.converters;
+
+import com.example.yosef.shastore.database.ProfileDb;
+import com.example.yosef.shastore.model.components.Profile;
+
+public class ProfileConverter {
+    public static Profile toProfile(ProfileDb profileDb) {
+        // TODO: implement
+        return null;
+    }
+
+    public static ProfileDb toProfileDb(Profile profile) {
+        // TODO: implement
+        return null;
+    }
+}
