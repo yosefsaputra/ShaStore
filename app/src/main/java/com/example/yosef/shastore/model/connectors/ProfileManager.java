9e97ec61785a1d7c7ebceba8666d862c5498881a
yosefsaputra <yosef.saputra@gmail.com>
2018-11-08 20:54:53 -0600

    To define the public boolean registerProfile(String username, String password)

--- app/src/main/java/com/example/yosef/shastore/model/connectors/ProfileManager.java
+++ app/src/main/java/com/example/yosef/shastore/model/connectors/ProfileManager.java
@@ -1,5 +1,8 @@
 package com.example.yosef.shastore.model.connectors;
 
+import com.example.yosef.shastore.database.AppDatabase;
+import com.example.yosef.shastore.model.components.Profile;
+
 public class ProfileManager {
     public boolean authenticateProfile(String username, String password) {
         // TODO: implement
@@ -7,7 +10,22 @@ public class ProfileManager {
     }
 
     public boolean registerProfile(String username, String password) {
-        // TODO: implement
+        String passwordSalt = "";
+        String passwordHash;
+
+        HashMachine hm = new GenericHashMachine();
+        hm.setText(password);
+        ((GenericHashMachine) hm).setSalt(passwordSalt);
+        hm.execute();
+
+        passwordHash = hm.getHashedText();
+
+        Profile newProfile = new Profile();
+        newProfile.setUsername(username);
+        newProfile.setPasswordHash(passwordHash);
+        newProfile.setPasswordSalt(passwordSalt);
+
+        AppDatabase.getDatabase().addProfile(newProfile);
         return false;
     }
 }
