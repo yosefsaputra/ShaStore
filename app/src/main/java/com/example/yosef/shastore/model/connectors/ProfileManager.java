95ac1139410ca8a4c1d39a638272741ce1692566
yosefsaputra <yosef.saputra@gmail.com>
2018-11-08 23:09:36 -0600

    To implement authenticateProfile() function in ProfileManager. To return appropriately in registerProfile()

--- app/src/main/java/com/example/yosef/shastore/model/connectors/ProfileManager.java
+++ app/src/main/java/com/example/yosef/shastore/model/connectors/ProfileManager.java
@@ -5,8 +5,24 @@ import com.example.yosef.shastore.model.components.Profile;
 
 public class ProfileManager {
     public boolean authenticateProfile(String username, String password) {
-        // TODO: implement
-        return false;
+        Profile profile = AppDatabase.getDatabase().getProfile(username);
+
+        if (profile != null) {
+            HashMachine hm = new GenericHashMachine();
+            hm.setText(password);
+            ((GenericHashMachine) hm).setSalt(profile.getPasswordSalt());
+            hm.execute();
+
+            String passwordHash = hm.getHashedText();
+
+            if (passwordHash == null) {
+                return false;
+            } else {
+                return passwordHash.equals(profile.getPasswordHash());
+            }
+        } else {
+            return false;
+        }
     }
 
     public boolean registerProfile(String username, String password) {
@@ -25,7 +41,6 @@ public class ProfileManager {
         newProfile.setPasswordHash(passwordHash);
         newProfile.setPasswordSalt(passwordSalt);
 
-        AppDatabase.getDatabase().addProfile(newProfile);
-        return false;
+        return AppDatabase.getDatabase().addProfile(newProfile);
     }
 }
